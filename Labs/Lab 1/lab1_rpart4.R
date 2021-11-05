4d240c9cef6d1d791e7a0bb9527b5062a0e94388
Jonathan Harris <harrij15@rpi.edu>
2021-11-05 11:43:05 -0400

    Abalone rpart4

--- Labs/Lab 1/lab1_rpart4.R
+++ Labs/Lab 1/lab1_rpart4.R
@@ -1,15 +1,17 @@
-fitK <- rpart(Kyphosis ~ Age + Number + Start, method="class", data=kyphosis)
+aba <- read.csv('~/DataAnalyticsFall2021_Jonathan_Harris/Data/abalone.csv')
+attach(aba)
+fitK <- rpart(Rings ~ Length + Diameter + Height + Whole.weight + Shucked.weight + Viscera.weight + Shell.weight, method="class", data=aba)
 
 printcp(fitK)
 plotcp(fitK) 
 summary(fitK) 
 
-plot(fitK, uniform=TRUE, main="Classification Tree for Kyphosis")
-text(fitK, use.n=TRUE, all=TRUE, cex=.8)
+plot(fitK, uniform=TRUE, main="Classification Tree for Abalone")
+text(fitK, all=TRUE, cex=.8)
 
-post(fitK, file = "kyphosistree.ps", title = "Classification Tree for Kyphosis") # might need to convert to PDF (distill)
+post(fitK, file = "abalone.ps", title = "Classification Tree for Abalone") # might need to convert to PDF (distill)
 
-pfitK<- prune(fitK, cp=   fitK$cptable[which.min(fitK$cptable[,"xerror"]),"CP"])
-plot(pfitK, uniform=TRUE, main="Pruned Classification Tree for Kyphosis")
-text(pfitK, use.n=TRUE, all=TRUE, cex=.8)
-post(pfitK, file = "ptree.ps", title = "Pruned Classification Tree for Kyphosis")
+pfitK<- prune(fitK, cp=fitK$cptable[which.min(fitK$cptable[,"xerror"]),"CP"])
+plot(pfitK, uniform=TRUE, main="Pruned Classification Tree for Abalone")
+text(pfitK, all=TRUE, cex=.8)
+post(pfitK, file = "ptree.ps", title = "Pruned Classification Tree for Abalone")
